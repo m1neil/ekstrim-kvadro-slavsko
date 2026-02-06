e912c562b13a3071c2c8b5b751083a396199120d
Eric <ericalex0207@gmail.com>
2026-02-06 21:17:41 +0200

    add: images, header

--- src/components/layout/menu/menu.js
+++ src/components/layout/menu/menu.js
@@ -1,16 +1,22 @@
 // Підключення функціоналу "Чортоги Фрілансера"
-import { addTouchAttr, bodyLockStatus, bodyLockToggle, FLS } from "@js/common/functions.js"
+import {
+	addTouchAttr,
+	bodyLockStatus,
+	bodyLockToggle,
+	FLS,
+} from '@js/common/functions.js'
 
 import './menu.scss'
 
 export function menuInit() {
-	document.addEventListener("click", function (e) {
-		if (bodyLockStatus && e.target.closest('[data-fls-menu]')) {
+	document.addEventListener('click', function (e) {
+		if (bodyLockStatus && e.target.closest('[data-burger]')) {
 			bodyLockToggle()
-			document.documentElement.toggleAttribute("data-fls-menu-open")
+			document.documentElement.toggleAttribute('data-fls-menu-open')
 		}
 	})
 }
 
-document.querySelector('[data-fls-menu]') ?
-	window.addEventListener('load', menuInit) : null
+document.querySelector('[data-burger]')
+	? window.addEventListener('load', menuInit)
+	: null
